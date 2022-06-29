4d6707613f25b1f4c81c18e537019ecb669d0677
Maxim Bureev <mvbureev@gmail.com>
2022-06-30 04:21:21 +0500

    final fix

--- configs/webpack/plugins/pluginMiniCssExtract.js
+++ configs/webpack/plugins/pluginMiniCssExtract.js
@@ -3,8 +3,8 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const config = {
   // Options similar to the same options in webpackOptions.output
   // both options are optional
-  filename: "[name].[contenthash].css",
-  chunkFilename: "[id].[contenthash].css",
+  filename: "[name].css",
+  chunkFilename: "[id].css",
 };
 
 exports.miniCssExtractPlugin = new MiniCssExtractPlugin(config);

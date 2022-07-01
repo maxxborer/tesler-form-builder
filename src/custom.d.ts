6814fd85292c510ab0ae50385c8f6478b5a1e348
Maxim Bureev <mvbureev@gmail.com>
2022-07-01 13:30:46 +0500

    fix

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -4,3 +4,9 @@ declare module "*.svg" {
   const content: any;
   export default content;
 }
+
+declare module "tesler-form-builder" {
+  function TeslerFormBuilder(): void; 
+  namespace TeslerFormBuilder { }
+  export = TeslerFormBuilder;
+}
